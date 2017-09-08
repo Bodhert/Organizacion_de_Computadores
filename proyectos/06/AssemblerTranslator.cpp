3d96929794b3fe055d4f356fade763066617ecb8
acordob5 <acordob5@31812d1e-1958-45e1-818e-752114580282>
2017-09-08 04:14:46 +0000

    working

--- proyectos/06/AssemblerTranslator.cpp
+++ proyectos/06/AssemblerTranslator.cpp
@@ -1,8 +1,45 @@
-#include<iostream>
+#include <iostream>
+#include <regex>
+#include <string>
+#include <stdio.h>
+
 using namespace std;
 
-
-int main()
+bool isAinstruction(string toParse)
 {
-  return 0;
+    regex Ainstruction("(\\\s*@)+(([[:digit:]]+)|([[:alpha:]]+))\\\s*");
+    return regex_match(toParse,Ainstruction);
+}
+
+void ReadAndParse(string file)
+{
+	freopen(file.c_str(),"r",stdin);
+	string input;
+	int lineCounter = 1;
+
+	while(getline(cin,input))
+	{
+	    if(isAinstruction(input))
+        {
+           cout << lineCounter << " A instrution" << endl;
+        }
+        else
+        {
+           cout << lineCounter << " Not A instrution" << endl;
+        }
+
+        lineCounter++;
+	}
+
+	fclose(stdin);
+
+}
+
+int main(int argc, char* argv[])
+{
+    int argvSize = sizeof(argv) / sizeof(char*);
+    if(argvSize == 1) ReadAndParse(argv[1]);
+    else cout << "The execution should be: .exe nameOfyourfile.asm" << endl;
+
+	return 0;
 }
