e0eb8ff743f5e6169e2b6cc9dc61690f7c553798
Alejandro Cordoba Bodhert <alejandro.cordoba.bodhert@gmail.com>
2017-09-28 05:15:14 +0000

    working

--- proyectos/07/main.cpp
+++ proyectos/07/main.cpp
@@ -1,9 +1,33 @@
 #include <iostream>
+#include "Parser.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+
+    Parser *parser = new Parser("in");
+    while(parser->hasMoreCommands())
+    {
+        parser->advance();
+        string commandType =  parser->commandType();
+        string arg1="";
+        int arg2 = -10;
+        if( commandType == "empty_line") continue;
+
+        if(commandType != "C_RETURN")
+        {
+             arg1 = parser->arg1() ;
+        }
+
+
+        if(commandType == "C_PUSH" || commandType == "C_POP"
+           || commandType == "C_FUNCTION" || commandType == "C_CALL")
+        {
+            arg2 = parser->arg2();
+        }
+
+        cout << commandType << " " << arg1 << " " << arg2 << endl;
+    }
     return 0;
 }
