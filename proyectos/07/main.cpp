195013fdbefa9e00aa27258ce2c1d34d7fb120c2
Alejandro Cordoba Bodhert <alejandro.cordoba.bodhert@gmail.com>
2017-09-29 16:16:34 +0000

    working

--- proyectos/07/main.cpp
+++ proyectos/07/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Parser.h"
+#include "CodeWriter.h"
 
 using namespace std;
 
@@ -7,27 +8,34 @@ int main()
 {
 
     Parser *parser = new Parser("in");
+    CodeWriter *codeWriter = new CodeWriter("in");
+
     while(parser->hasMoreCommands())
     {
         parser->advance();
         string commandType =  parser->commandType();
         string arg1="";
         int arg2 = -10;
-        if( commandType == "empty_line") continue;
+
+        if(commandType == "empty_line") continue;
 
         if(commandType != "C_RETURN")
         {
-             arg1 = parser->arg1() ;
+             arg1 = parser->arg1();
+             if(commandType == "C_ARITHMETIC") codeWriter->writeAritmetic(arg1);
         }
-
 
         if(commandType == "C_PUSH" || commandType == "C_POP"
            || commandType == "C_FUNCTION" || commandType == "C_CALL")
         {
             arg2 = parser->arg2();
+            if(commandType == "C_PUSH")
+            {
+                codeWriter->writePushPop(commandType,arg1,arg2);
+            }
         }
 
-        cout << commandType << " " << arg1 << " " << arg2 << endl;
+        //cout << commandType << " " << arg1 << " " << arg2 << endl;
     }
     return 0;
 }
